6191e3029b7567710957cc688d06bfd9b1203f57
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2023-03-15 13:10:07 -0300

    codigo comentado
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- Practico1/Practico1.c
+++ Practico1/Practico1.c
@@ -1,36 +1,31 @@
-// #include <wiringPi.h>
-#include <stdio.h>
-#include <time.h>
+#include <wiringPi.h> // Inicializo libreria para controlar los pines
+#include <stdio.h>    // Inicializo libreria para imprimir por pantalla
+#include <time.h>     // Inicializo libreria para calcular los tiempos
 
-#define LED 17
+#define LED 17 // El pin donde se conecta el Led
 
 int main(void)
 {
-  clock_t start, stop;
-  int i = 0;
+  clock_t start; // Declaro variable para guardar los cloks
+  int i = 0;     // Declaro variable hacer el while infinito
 
-  time_t rawtime;
-  struct tm *timeinfo;
-
-  // wiringPiSetupGpio();
-  // pinMode(LED, OUTPUT);
-  start = clock();
+  wiringPiSetupGpio();  // Establezco conexion con los pines
+  pinMode(LED, OUTPUT); // Declaro al pin 17 como pin de salida
+  start = clock();      // Guardo la cantidad de clocks transcurridos
   while (1 > i)
   {
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    if ((((double)(clock() - start) / (CLOCKS_PER_SEC))) <= 0.5)
+    if ((((double)(clock() - start) / (CLOCKS_PER_SEC))) <= 0.5) //Voy contando los clocks hasta que pase medio segundo
     {
-      //  digitalWrite(LED, 1);
-      printf("Prendo%d\n",timeinfo->tm_sec);
+      digitalWrite(LED, 1); // Prendo el Led
+      printf("Prendo\n");   // Imprimo en pantalla
     }
     else
     {
-      //  digitalWrite(LED, 0);
-      printf("Apago%d\n",timeinfo->tm_sec);
-      if ((((double)(clock() - start) / (CLOCKS_PER_SEC))) >= 1)
+      digitalWrite(LED, 0); // Apago el Led
+      printf("Apago\n");    // Imprimo en pantalla
+      if ((((double)(clock() - start) / (CLOCKS_PER_SEC))) >= 1) //Voy contando los clocks hasta que pase un segundo
       {
-        start = clock();
+        start = clock(); // "Reseteo los segundos" para volver a prender el led
       }
     }
   }
