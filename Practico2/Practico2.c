5115b453637e4f125c83f8722c3fdb885367128d
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2023-03-20 19:40:23 -0300

    empezado el tp 2
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- Practico2/Practico2.c
+++ Practico2/Practico2.c
@@ -1,45 +1,50 @@
-#include <Wire.h>
-#include <Adafruit_BMP280.h>
-#include "AHT10.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
-AHT10 aht;
-Adafruit_BMP280 bmp;
+#define AHT10_ADDRESS 0x38 // AHT10 I2C address
 
-void setup()
+int main()
 {
-    Serial.begin(9600);
-
-    aht.begin();
-
-    if (!bmp.begin())
+    int file;
+    char *filename = "/dev/i2c-1"; // I2C bus device file
+    if ((file = open(filename, O_RDWR)) < 0)
     {
-        Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
-        while (1)
-            ;
+        printf("Failed to open I2C bus %s\n", filename);
+        exit(1);
     }
 
-    delay(1000);
-    Serial.println("| Temp AHT10 | Temp BMP | Presión BMP | Humedad AHT10 |");
-}
+    if (ioctl(file, I2C_SLAVE, AHT10_ADDRESS) < 0)
+    {
+        printf("Failed to select AHT10 sensor\n");
+        exit(1);
+    }
 
-void loop()
-{
+    // Send command to measure temperature and humidity
+    char command[3] = {0xAC, 0x33, 0x00};
+    write(file, command, 3);
 
-    float humedad = aht.readHumidity();
-    float tempaht10 = aht.readTemperature();
-    float tempbmp = bmp.readTemperature();
-    float presion = (bmp.readPressure() / 100);
+    usleep(50000); // Wait for measurement to complete (50ms)
 
-    Serial.print("| ");
-    Serial.print(tempaht10);
-    Serial.print(" ºC  | ");
-    Serial.print(tempbmp);
-    Serial.print(" ºC | ");
-    Serial.print(presion);
-    Serial.print(" hPa  |    ");
-    Serial.print(humedad);
-    Serial.print(" %   |");
+    // Read temperature and humidity data
+   
+    char data[6];
+    read(file, data, 6);
+    float cur_temp, ctmp;
 
-    Serial.println();
-    delay(1000);
+    cur_temp = (((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]);
+    cur_temp = ((cur_temp * 200) / 1048576) - 50;
+    printf("Temperature: %2.2f\n", cur_temp);
+    ctmp = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4;
+    ctmp = ctmp * 100 / 1048576;
+    printf("Humidity: %1.f %\n", ctmp);
+
+    // printf("Temperature: %d.%d C\n", temperature / 10, temperature % 10);
+    // printf("Humidity: %d%%\n", humidity);
+
+    close(file);
+    return 0;
 }
