2df50528296ffe8c7d716bdc2e8233fbcd5fa408
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2023-03-28 08:56:19 -0300

    Arreglando un error de archivos
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- Practico3/Practico3.c
+++ Practico3/Practico3.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <wiringPi.h>
-
+#include <softPwm.h>
 // Definimos el pin de salida que se utilizará para la señal PWM
-#define PWM_PIN 18
+#define PWM_PIN 1
 
-int main(void) {
+int main(void)
+{
 
     float grados;
     grados = 0;
     // Inicializamos la biblioteca WiringPi y configuramos el pin de salida como PWM
-    wiringPiSetupGpio();
+    // wiringPiSetupGpio();
+    // pinMode(PWM_PIN, PWM_OUTPUT);
+    wiringPiSetup();
     pinMode(PWM_PIN, PWM_OUTPUT);
-
+    digitalWrite(PWM_PIN, 0);
+    softPwmCreate(PWM_PIN, 0, 200);
     // Configuramos la frecuencia de la señal PWM
-    pwmSetMode(PWM_MODE_MS);
-    pwmSetClock(50);
-    pwmSetRange(20);
-    float milisegundos;
+    // pwmSetMode(PWM_MODE_MS);
+    // pwmSetClock(384);
+    // pwmSetRange(1000);
+    float microsegundos;
 
-    while(1) {
-        printf("Incerte la cantidad de grados que debe girar el servo \n")
-        scanf("%lf", &grados);
+    while (1)
+    {
+        printf("Incerte la cantidad de grados que debe girar el servo \n");
+        scanf("%f", &grados);
         // Enviamos el pulso mínimo al servo
-        milisegundos = (grados*0.5)/90;
-
-
-        pwmWrite(PWM_PIN, milisegundos);
+        microsegundos = (((1 / 180) * grados) + 1) * 10;
+        printf("microsegundos %f\n", microsegundos);
+        softPwmWrite(PWM_PIN, (microsegundos));
+        // pwmWrite(PWM_PIN, microsegundos);
 
         // Esperamos un tiempo (por ejemplo, 1 segundo)
         delay(1000);
@@ -35,6 +40,6 @@ int main(void) {
 }
 
 /*
-Este programa utiliza la biblioteca WiringPi para generar una señal PWM de 50 Hz en el pin 18 de la Raspberry Pi 3. 
+Este programa utiliza la biblioteca WiringPi para generar una señal PWM de 50 Hz en el pin 18 de la Raspberry Pi 3.
 Luego, envía pulsos de ancho variable (entre 500 y 2500 microsegundos) al servo SG90 utilizando la función pwmWrite().
 */
