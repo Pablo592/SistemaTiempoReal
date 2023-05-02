d42c5d3e7b2ae5ca2ca2bafa375a4f69e5543ebd
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2023-05-02 14:52:38 -0300

    Implementada la memoria compartida
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- Proyecto-Integrador/proyecto.c
+++ Proyecto-Integrador/proyecto.c
@@ -1,10 +1,17 @@
-#include <stdio.h> // Inicializo libreria para imprimir por pantalla
-#include <time.h>  // Inicializo libreria para calcular los tiempos
+#include <stdio.h>
+#include <time.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <sys/mman.h>
 
 void *lectorDeArchivo();
 void *monitoreaSensorHumedadTemperatura();
@@ -13,19 +20,31 @@ void *activaServomotor();
 void *monitoreaCambiosArchivo();
 void *monitoreaPulsador();
 
-int tempMax;
-int humMin;
-int horaRiego;
-int minutoRiego;
-int duracionMinutosRiego;
-int tiempoAnticipacionAlarma;
-int duracionMinutosAlarma;
+struct parameters
+{
+    int tempMax;
+    int humMin;
+    int horaRiego;
+    int minutoRiego;
+    int duracionMinutosRiego;
+    int tiempoAnticipacionAlarma;
+    int duracionMinutosAlarma;
+};
+
+///////////Shared memory////////
+int shared_fd_parameters = -1;
+struct parameters *ptr_parameters = NULL;
+#define SH_SIZE_TIEMPO_PULSADO 1
+///////////////////////////////////////
 
 // AUX FUNCTIONS
 bool starts_with(const char *str, const char *prefix);
+int init_shared_resource();
 
 int main(void)
 {
+    init_shared_resource();
+
     pthread_t hilo1; //
     pthread_t hilo2; // Inicializo los hilos
     pthread_t hilo3; //
@@ -51,8 +70,8 @@ int main(void)
 
 void *lectorDeArchivo()
 {
-    FILE *file_pointer; //Declaro un puntero que va a ir apuntando al archivo fila por fila
-    char str[150];  // Declaro una variable que va a contener las filas del archivo
+    FILE *file_pointer;                        // Declaro un puntero que va a ir apuntando al archivo fila por fila
+    char str[150];                             // Declaro una variable que va a contener las filas del archivo
     file_pointer = fopen("riego.config", "r"); // Apunto el puntero al inicio del archivo
 
     if (file_pointer == NULL)
@@ -71,45 +90,45 @@ void *lectorDeArchivo()
             char *ptr = strtok(str, ":"); // La posicion 0 del split(":")
             ptr = strtok(NULL, ":");      // La posicion 1 del split(":")
 
-            if (starts_with(str, "TempMax"))    // Asigno los valores a las variables correspondientes
+            if (starts_with(str, "TempMax")) // Asigno los valores a las variables correspondientes
             {
-                tempMax = atoi(ptr);        // Parceo de char[] a int
-                printf("'%d'\n", tempMax);
+                ptr_parameters->tempMax = atoi(ptr); // Parceo de char[] a int y guardo el numero en la estructura alojada en la memoria compartida
+                printf("'%d'\n", ptr_parameters->tempMax); // Imprimo el valor ajodado en la memoria compartida
             }
             else if (starts_with(str, "HumMin"))
             {
-                humMin = atoi(ptr);
-                printf("'%d'\n", humMin);
+                ptr_parameters->humMin = atoi(ptr);
+                printf("'%d'\n", ptr_parameters->humMin);
             }
             else if (starts_with(str, "HoraRiego"))
             {
-                horaRiego = atoi(ptr);
-                printf("'%d'\n", horaRiego);
+                ptr_parameters->horaRiego = atoi(ptr);
+                printf("'%d'\n", ptr_parameters->horaRiego);
             }
             else if (starts_with(str, "MinutoRiego"))
             {
-                minutoRiego = atoi(ptr);
-                printf("'%d'\n", minutoRiego);
+                ptr_parameters->minutoRiego = atoi(ptr);
+                printf("'%d'\n", ptr_parameters->minutoRiego);
             }
             else if (starts_with(str, "DuracionMinutosRiego"))
             {
-                duracionMinutosRiego = atoi(ptr);
-                printf("'%d'\n", duracionMinutosRiego);
+                ptr_parameters->duracionMinutosRiego = atoi(ptr);
+                printf("'%d'\n", ptr_parameters->duracionMinutosRiego);
             }
             else if (starts_with(str, "TiempoAnticipacionAlarma"))
             {
-                tiempoAnticipacionAlarma = atoi(ptr);
-                printf("'%d'\n", tiempoAnticipacionAlarma);
+                ptr_parameters->tiempoAnticipacionAlarma = atoi(ptr);
+                printf("'%d'\n", ptr_parameters->tiempoAnticipacionAlarma);
             }
             else if (starts_with(str, "DuracionMinutosAlarma"))
             {
-                duracionMinutosAlarma = atoi(ptr);
-                printf("'%d'\n", duracionMinutosAlarma);
+                ptr_parameters->duracionMinutosAlarma = atoi(ptr);
+                printf("'%d'\n", ptr_parameters->duracionMinutosAlarma);
             }
         }
     }
 
-    fclose(file_pointer);   // Cierro el archivo
+    fclose(file_pointer); // Cierro el archivo
     return NULL;
 }
 
@@ -148,4 +167,33 @@ bool starts_with(const char *str, const char *prefix)
         return false;
     }
     return strncmp(str, prefix, prefix_len) == 0;
+}
+
+int init_shared_resource()
+{
+
+    shared_fd_parameters = shm_open("/shm1", O_CREAT | O_RDWR, 0600);
+
+    if (shared_fd_parameters < 0)
+    {
+        fprintf(stderr, "ERROR: Failed to create shared memory TIEMPO_PULSADO: %s\n", strerror(errno));
+        exit(1);
+    }
+    fprintf(stdout, "Shared memory is created with fd: %d\n", shared_fd_parameters);
+
+    if (ftruncate(shared_fd_parameters, SH_SIZE_TIEMPO_PULSADO * sizeof(struct parameters)) < 0)
+    {
+        fprintf(stderr, "ERROR: Truncation failed: %s\n", strerror(errno));
+        return 1;
+    }
+
+    void *map_parameters = mmap(0, SH_SIZE_TIEMPO_PULSADO, PROT_WRITE, MAP_SHARED, shared_fd_parameters, 0);
+
+    if (map_parameters == MAP_FAILED)
+    {
+        fprintf(stderr, "ERROR: Mapping failed: %s\n", strerror(errno));
+        return 1;
+    }
+
+    ptr_parameters = (struct parameters *)map_parameters;
 }
