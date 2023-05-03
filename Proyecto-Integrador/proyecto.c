fd0ee2f965a7c849e75b2378e8674b4b29d7b5cd
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2023-05-03 18:09:34 -0300

    probando el codigo comentando lo referente a los sensores
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- Proyecto-Integrador/proyecto.c
+++ Proyecto-Integrador/proyecto.c
@@ -1,4 +1,4 @@
-#include <wiringPi.h>
+// #include <wiringPi.h>
 #include <stdio.h>
 #include <time.h>
 #include <pthread.h>
@@ -146,44 +146,47 @@ void *lectorDeArchivo()
 
 void *monitoreaSensorHumedadTemperatura()
 {
-    wiringPiSetupGpio();
-    int file;
-    char *filename = "/dev/i2c-1";           // I2C bus device file
-    if ((file = open(filename, O_RDWR)) < 0) // Verifico la conexion con la interfaz I2C
-    {
-        printf("Failed to open I2C bus %s\n", filename);
-        exit(1);
-    }
+    /*  wiringPiSetupGpio();
+      int file;
+      char *filename = "/dev/i2c-1";           // I2C bus device file
+      if ((file = open(filename, O_RDWR)) < 0) // Verifico la conexion con la interfaz I2C
+      {
+          printf("Failed to open I2C bus %s\n", filename);
+          exit(1);
+      }
 
-    if (ioctl(file, I2C_SLAVE, AHT10_ADDRESS) < 0) // Verifico la conexion con el sensor AHT10
-    {
-        printf("Failed to select AHT10 sensor\n");
-        exit(1);
-    }
-
+      if (ioctl(file, I2C_SLAVE, AHT10_ADDRESS) < 0) // Verifico la conexion con el sensor AHT10
+      {
+          printf("Failed to select AHT10 sensor\n");
+          exit(1);
+      }
+  */
     // Send command to measure temperature and humidity
     char command[3] = {0xAC, 0x33, 0x00}; // Busco en memoria la humedad y temperatura detectada por el sensor
 
-    float cur_temp, ctmp;
+    double cur_temp, ctmp;
     while (1) // El sensor recopila datos sin parar
     {
-        write(file, command, 3);
-        usleep(50000); // Se espera un tiempo a que las mediciones puedan completarse (50ms)
-        char data[6];
-        read(file, data, 6);
+        /*    write(file, command, 3);
+            usleep(50000); // Se espera un tiempo a que las mediciones puedan completarse (50ms)
+            char data[6];
+            read(file, data, 6);
 
-        cur_temp = (((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]);
-        cur_temp = ((cur_temp * 200) / 1048576) - 50;
-        printf("Temperature: %2.2f\n", cur_temp); // Se imprime la temperatura monitoreada por consola
-        ctmp = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4;
-        ctmp = ctmp * 100 / 1048576;
-        printf("Humidity: %1.f %\n", ctmp); // Se imprime la humedad monitoreada por consola
-
+            cur_temp = (((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]);
+            cur_temp = ((cur_temp * 200) / 1048576) - 50;
+            printf("Temperature: %2.2f\n", cur_temp); // Se imprime la temperatura monitoreada por consola
+            ctmp = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4;
+            ctmp = ctmp * 100 / 1048576;
+            printf("Humidity: %1.f %\n", ctmp); // Se imprime la humedad monitoreada por consola
+    */
+        cur_temp += 0.1;
+        ctmp += 0.1;
+        sleep(1);
         ptr_sensor->temperatura = cur_temp;
         ptr_sensor->humedad = ctmp;
     }
 
-    close(file);
+    //   close(file);
 
     return NULL;
 }
@@ -195,6 +198,12 @@ void *activaAlarma()
 
 void *activaServomotor()
 {
+    while (1)
+    {
+        sleep(1);
+        printf("Temperatura: %lf\n", ptr_sensor->temperatura);
+        printf("Humedad: %lf\n", ptr_sensor->humedad);
+    }
     return NULL;
 }
 
